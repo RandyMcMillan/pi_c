396639b38f3aa0b68b12f6e536f51d7ee0c0d767
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-12-01 09:51:54 -0500

    init commit

---
+++ GNUmakefile
@@ -0,0 +1,39 @@
+default:
+	$(MAKE) clean
+	@gcc pi.c -o pi
+test: default tests
+.PHONY:tests
+tests: pi
+	@mkdir -p logs
+	@`pwd`/./pi 1      > `pwd`/logs/1.log
+	@`pwd`/./pi 5      > `pwd`/logs/5.log
+	@`pwd`/./pi 9      > `pwd`/logs/9.log
+	@`pwd`/./pi 13     > `pwd`/logs/13.log
+	@`pwd`/./pi 17     > `pwd`/logs/17.log
+	@`pwd`/./pi 21     > `pwd`/logs/21.log
+	@`pwd`/./pi 25     > `pwd`/logs/25.log
+	@`pwd`/./pi 29     > `pwd`/logs/29.log
+	@`pwd`/./pi 33     > `pwd`/logs/33.log
+	@`pwd`/./pi 37     > `pwd`/logs/37.log
+	@`pwd`/./pi 41     > `pwd`/logs/41.log
+	@`pwd`/./pi 45     > `pwd`/logs/45.log
+	@`pwd`/./pi 49     > `pwd`/logs/49.log
+	@`pwd`/./pi 53     > `pwd`/logs/53.log
+	@`pwd`/./pi 57     > `pwd`/logs/57.log
+	@`pwd`/./pi 61     > `pwd`/logs/61.log
+	@`pwd`/./pi 65     > `pwd`/logs/65.log
+	@`pwd`/./pi 69     > `pwd`/logs/69.log
+	@`pwd`/./pi 73     > `pwd`/logs/73.log
+	@`pwd`/./pi 77     > `pwd`/logs/77.log
+	@`pwd`/./pi 81     > `pwd`/logs/81.log
+	@`pwd`/./pi 85     > `pwd`/logs/85.log
+	@`pwd`/./pi 89     > `pwd`/logs/89.log
+	@`pwd`/./pi 93     > `pwd`/logs/93.log
+	@`pwd`/./pi 97     > `pwd`/logs/97.log
+	@`pwd`/./pi 101    > `pwd`/logs/101.log
+	@`pwd`/./pi 1001   > `pwd`/logs/1001.log
+	@`pwd`/./pi 10001  > `pwd`/logs/10001.log
+	@git diff
+clean:
+	@rm -f pi
+	@rm -f *.out
---
+++ pi.c
@@ -0,0 +1,164 @@
+/* Print pi to n decimal places (default 1000): pi n */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+	unsigned short chunk;
+	unsigned short index;
+} chunk_index;
+
+void swap(unsigned short* xp, unsigned short* yp);
+void bubbleSort(unsigned short arr[], unsigned short arr2[], int n);
+void printArray(unsigned short arr[], int size);
+void populate_chunk_index(unsigned short arr[], chunk_index ci[], int size);
+
+/* Print pi as an array of n digits in base 10000 */
+void print(unsigned short *pi, unsigned short *list, unsigned short *gt_list, int n) {
+  int i;
+  printf("%d", pi[0]);
+  printf("%d.\n", pi[1]);
+  for (i=2; i<n-1; ++i)
+    if (pi[i] <= 2048){
+        list[i] = pi[i];
+        gt_list[i] = 0;
+    }else{
+        list[i] = 0;
+        gt_list[i] = pi[i];
+    }
+  printf("\n");
+}
+
+/* Compute pi to B bits precision by the Spigot algorithm given by
+Rabinowitz and Wagon, Am. Math. Monthly, March 1995, 195-203.
+
+   pi = 4;
+   for (i = B; i>0; --i)
+     pi = 2 + pi * i / (2*i+1)
+
+pi is represented by a base 10000 array of digits with 2 digits before
+the decimal point (pi[0], pi[1]), and one extra digit (pi[n-1]) at
+the end to allow for roundoff error, which is not printed.  Note that a
+base 10 digit is equivalent to log(10)/log(2) = 3.322 bits.
+
+For shorter versions, see
+http://www1.physik.tu-muenchen.de/~gammel/matpack/html/Mathematics/Pi.html
+http://numbers.computation.free.fr/Constants/TinyPrograms/tinycodes.html
+
+and for an explanation of how they work, see
+Unbounded Spigot Algorithms for the Digits of Pi,
+Jeremy Gibbons, University of Oxford, 2004,
+http://web.comlab.ox.ac.uk/oucl/work/jeremy.gibbons/publications/spigot.pdf
+
+*/
+
+void swap(unsigned short* xp, unsigned short* yp)
+{
+	unsigned short temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+// A function to implement bubble sort
+void bubbleSort(unsigned short arr[], unsigned short arr2[], int n)
+{
+	int i, j;
+	for (i = 0; i < n - 1; i++)
+
+		// Last i elements are already in place
+		for (j = 0; j < n - i - 1; j++)
+			if (arr[j] > arr[j + 1])
+				swap(&arr[j],  &arr[j + 1]);
+				swap(&arr2[j], &arr2[j + 1]);
+}
+
+/* Function to print an array */
+void printArray(unsigned short arr[], int size)
+{
+	int i;
+	for (i = 2; i < size-1; i++)
+		//printf("%04d:     ", arr[i]);
+		printf("%04d:%04d ", arr[i], i);
+	printf("\n");
+}
+void populate_chunk_index(unsigned short arr[], chunk_index ci[], int size)
+{
+	int i;
+	for (i = 2; i < size-1; i++){
+		ci[i].chunk = arr[i];
+		ci[i].index = i;
+		//printf("i=%04d\n",i);
+		//printf("arr[i]=%04d\n", arr[i]);
+		printf("%04d:%04d:%04d\n", ci[i].chunk, ci[i].index, size);
+		};
+
+}
+
+int main(int argc, char** argv) {
+
+  int n = argc > 1 ? (atoi(argv[1])+3)/4+3 : 253;  /* number of pi digits */
+  unsigned short *pi       = (unsigned short*) malloc(n * sizeof(unsigned short));
+  unsigned short *list     = (unsigned short*) malloc(n * sizeof(unsigned short));
+  unsigned short *gt_list  = (unsigned short*) malloc(n * sizeof(unsigned short));
+
+  chunk_index *chunk_list  = (chunk_index*)    malloc(n * sizeof(chunk_index));
+
+  div_t d;
+  int i, j, t1, t2, t3;
+
+  /* pi = 4  */
+  memset(pi,      0, n*sizeof(unsigned short));
+  memset(list,    0, n*sizeof(unsigned short));
+  memset(gt_list, 0, n*sizeof(unsigned short));
+  pi[1]=4;
+  list[1]=4;
+  gt_list[1]=4;
+
+  /* for i = B down to 1 */
+  for (i=(int)(3.322*4*n); i>0; --i) {
+
+    /* pi *= i; */
+    t1 = 0;
+    t2 = 0;
+    t3 = 0;
+    for (j=n-1; j>=0; --j) {  /* pi *= i; */
+      t1 += pi[j] * i;
+      t2 += list[j] * i;
+      t3 += gt_list[j] * i;
+      pi[j]      = t1 % 10000;
+      list[j]    = t2 % 10000;
+      gt_list[j] = t3 % 10000;
+      t1 /= 10000;
+      t2 /= 10000;
+      t3 /= 10000;
+    }
+
+    /* pi /= (2*i+1) */
+    d.quot = d.rem = 0;
+    for (j=0; j<n; ++j) {  /* pi /= 2*i+1; */
+      d = div(pi[j]+d.rem*10000, i+i+1);
+      pi[j]      = d.quot;
+      list[j]    = d.quot;
+      gt_list[j] = d.quot;
+    }
+
+    /* pi += 2 */
+    pi[1] += 2;
+    list[1] += 2;
+    gt_list[1] += 2;
+  }
+
+  //bubbleSort(pi, list, n);
+  // printf("Sorted array: \n");
+
+  printArray(pi,   n);
+  //printArray(list, n);
+  //printArray(gt_list, n);
+  populate_chunk_index(pi, chunk_list, n);
+
+  printf("\n");
+
+  return 0;
+}
+
