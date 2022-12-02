218e555af29a13eb7ade37a5f03423ceaab1de8d
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-12-02 11:42:41 -0500

    constants.c:

--- pi.c
+++ pi.c
@@ -6,6 +6,7 @@
 #include "print_pi.c"
 #include "populate_chunk_index.c"
 #include "bubble_sort.c"
+#include "constants.c"
 
 #define BASE 1000
 
@@ -35,29 +36,6 @@ void print(unsigned long *pi, unsigned long *list, unsigned long *gt_list, int n
 		}
 	printf("\n");
 }
-
-/* Compute pi to B bits precision by the Spigot algorithm given by
-Rabinowitz and Wagon, Am. Math. Monthly, March 1995, 195-203.
-
-   pi = 4;
-   for (i = B; i>0; --i)
-     pi = 2 + pi * i / (2*i+1)
-
-pi is represented by a base BASE array of digits with 2 digits before
-the decimal point (pi[0], pi[1]), and one extra digit (pi[n-1]) at
-the end to allow for roundoff error, which is not printed.  Note that a
-base 10 digit is equivalent to log(10)/log(2) = 3.322 bits.
-
-For shorter versions, see
-http://www1.physik.tu-muenchen.de/~gammel/matpack/html/Mathematics/Pi.html
-http://numbers.computation.free.fr/Constants/TinyPrograms/tinycodes.html
-
-and for an explanation of how they work, see
-Unbounded Spigot Algorithms for the Digits of Pi,
-Jeremy Gibbons, University of Oxford, 2004,
-http://web.comlab.ox.ac.uk/oucl/work/jeremy.gibbons/publications/spigot.pdf
-
-*/
 
 /* Function to print an array */
 void printArray(unsigned long arr[], int size)
