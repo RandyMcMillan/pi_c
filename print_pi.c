cc7d18513a0c491ea36fc775472acd69baaca81f
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-12-02 11:35:28 -0500

    print_pi.c:

---
+++ print_pi.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Print pi as an array of n digits in base BASE */
+void print_pi(unsigned long *pi, unsigned long *list, unsigned long *gt_list, int n) {
+
+	int i;
+	//printf("print\n");
+	printf("\n");
+	//printf("%lu", pi[0]);
+	printf("%1lu.\n", pi[1]);
+	for (i=2; i<n-1; ++i)
+		if (pi[i] <= 2048){
+			printf("%04lu\n", pi[i]);
+		}else{}
+	printf("\n");
+}
